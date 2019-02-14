2e580120144e5e64cfa6b2f7e7c8f9f0a7559d01
manos <manos.emmanouil.deliverybludprints.com>
2019-02-14 15:21:57 +0000

    add buttons to use Google auth api in order to log in or log out using class state

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -2,23 +2,60 @@ import React from 'react';
 
 
 class GoogleAuth extends React.Component{
+    state = {isSignedIn: null};
 
     componentDidMount(){
         window.gapi.load('client:auth2', ()=>{
             window.gapi.client.init({
                 clientId: '672501869896-b14vhhr686u2ipoou0b0v553ohpj0pk3.apps.googleusercontent.com',
                 scope: 'email'
+            }).then(()=>{
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+                this.auth.isSignedIn.listen(this.onAuthChange)
             })
         });
 
     }
 
+    onAuthChange = () =>{
+        this.setState({
+            isSignedIn: this.auth.isSignedIn.get()
+        })
+    };
+
+    onSignIn = () =>{
+        this.auth.signIn();
+    };
+
+    onSignOut = () =>{
+      this.auth.signOut();
+    };
+
+    renderAuthButton(){
+        if(this.state.isSignedIn === null){
+            return null;
+        } else if( this.state.isSignedIn){
+            return (
+                <button className={"ui red google button"} onClick={this.onSignOut}>
+                    <i className={"google icon"}/>
+                    Sign out
+                </button>
+            );
+        } else {
+            return (
+                <button className={"ui green google button"} onClick={this.onSignIn}>
+                    <i className={"google icon"}/>
+                    Sign in
+                </button>
+            );
+
+        }
+
+    }
+
     render(){
-        return(
-            <div>
-                Google Auth
-            </div>
-        );
+        return this.renderAuthButton();
     }
 }
 
