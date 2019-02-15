b2ec9a9b628f40f0b9adf31df5fb82a1dad65aa9
manos <manos.emmanouil.deliverybludprints.com>
2019-02-15 15:18:22 +0000

    1) add react-form moduel 2) create stream form created 3) add validation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import {Provider} from 'react-redux';
-import {createStore, applyMiddleware, compose} from 'redux';
+import {Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose} from 'redux';
 import reducers from './reducers';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
