2829a2ffff49ffbb4a723c8973cc9fb41ae85aa3
emmanouil e <emmanouil.e25@gmail.com>
2019-03-02 16:22:31 +0000

    1) crate json-server 2) create actions to CRUD streams 3) use create action to dispatch it and save it to db

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import {Field, reduxForm} from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream} from '../../actions'
 
 class StreamCreate extends React.Component{
 
@@ -26,8 +28,8 @@ class StreamCreate extends React.Component{
         );
     };
 
-    onSubmit( formProps ){
-        console.log(formProps);
+    onSubmit = ( formValues ) =>{
+       this.props.createStream(formValues);
     }
 
     render(){
@@ -63,7 +65,13 @@ const validate = ( formValues ) => {
     return errors;
 };
 
-export default reduxForm({
+const formWrapped = reduxForm({
     form: 'streamCreate',
     validate
 })(StreamCreate);
+
+export default connect(
+    null,{
+        createStream
+    }
+)(formWrapped);
