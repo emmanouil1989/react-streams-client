f2f5bbe8462ca598d55e1c2f68fe715dec96c0d8
emmanouile <manos.emmanouil@deliveryblueprints.com>
2019-06-07 14:23:53 +0100

    add edit/delete button which shows only for logged in user and a create button which should redirect to the main page

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { connect } from 'react-redux';
-import { fetchStreams } from "../../actions";
+import {connect} from 'react-redux';
+import {fetchStreams} from "../../actions";
+import {Link} from 'react-router-dom';
 
 class StreamList extends React.Component{
 
@@ -8,13 +9,67 @@ class StreamList extends React.Component{
         this.props.fetchStreams();
     }
 
+    renderAdmin = (stream) =>{
+        if(stream.userId === this.props.currentUserId){
+            return(
+              <div className={"right floated content"}>
+                  <button className={"ui button primary"}>
+                      edit
+                  </button>
+                  <button className={"ui button negative"}>
+                      delete
+                  </button>
+              </div>
+            );
+        }
+    };
+
+    renderList(){
+        return this.props.streams.map(stream =>{
+           return(
+               <div className={"item"} key={stream.id}>
+                   {this.renderAdmin(stream)}
+                   <i className={"large middle aligned icon camera"}/>
+                   <div className={"content"}>
+                       {stream.title}
+                       <div className={"description"}>{ stream.description }</div>
+                   </div>
+
+               </div>
+           )
+
+        });
+    }
+
+    renderCreate = ()=>{
+        if(this.props.isSignedIn){
+            return(  <div style={{textAlign: 'right'}}>
+                    <Link to={'/streams/new'} className={"ui button primary"} >
+                        Create Stream
+                    </Link>
+                </div>
+            )
+        }
+    };
 
     render(){
         return(
-            <div>StremList</div>
+            <div>
+                <h2>Stream</h2>
+                <div className={"ui celled list"}>{this.renderList()}</div>
+                {this.renderCreate()}
+            </div>
         )
     }
 }
 
 
-export default connect(null,{ fetchStreams })(StreamList);
+const mapStateToProps = (state) =>{
+    return {
+        streams: Object.values(state.streams),
+        currentUserId: state.auth.userId,
+        isSignedIn: state.auth.isSignedIn
+    }
+};
+
+export default connect(mapStateToProps,{ fetchStreams })(StreamList);
