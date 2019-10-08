b2af90eb44643f8d20cd9617493b676e427d7385
emmanouile <manos.emmanouil@deliveryblueprints.com>
2019-10-08 08:51:07 +0100

    add generic modal and render it into delete stream.

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import Modal from '../Modal';
+import history from '../../history'
 const StreamDelete =()=>{
+    const actions = (
+        // react fragment allow to group jsx elements and not produce div html
+        <>
+            <button className={"ui negative button"}>Delete</button>
+            <button className={"ui button"}>Cancel</button>
+        </>
+    );
+
     return(
-        <div>StreamDelete</div>
+        <div>
+            StreamDelete
+            <Modal
+                title={"Delete Stream"}
+                content={'Are you sure you want to delete this stream?'}
+                actions={actions}
+                onDismiss={()=> history.push('/')}
+            />
+        </div>
     )
 
 };
