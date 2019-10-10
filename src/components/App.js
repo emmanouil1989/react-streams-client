409076f5fe1fb7253a112bd0c37be0853578ef5d
emmanouile <manos.emmanouil@deliveryblueprints.com>
2019-10-10 09:05:35 +0100

    add switch to solve the navigation problem.

--- src/components/App.js
+++ src/components/App.js
@@ -9,17 +9,19 @@ import Header from './Header';
 import history from '../history'
 
 const App = ()=>{
+    //react router dom return a route which mach the exact path. So because /streams/new and /stream/:id are the same, react router dom will return both. They are the same because of the wildcard :id. Switch solve that problem by return the first match only.
     return(
         <div className={"ui container"}>
             <Router history={history}>
                 <div>
                     <Header/>
+
                     <Switch>
                         <Route path={"/"} exact component={ StreamList }/>
                         <Route path={"/streams/new"} exact component={ StreamCreate }/>
                         <Route path={"/streams/edit/:id"} exact component={ StreamEdit }/>
                         <Route path={"/stream/delete/:id"} exact component={ StreamDelete }/>
-                        <Route path={"/streams/show/:id"} exact component={ StreamShow }/>
+                        <Route path={"/streams/:id"} exact component={ StreamShow }/>
                     </Switch>
                 </div>
             </Router>
